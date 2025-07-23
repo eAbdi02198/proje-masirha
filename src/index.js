91937acdadfda5f7a8233b0a2512177e91ccc2d7
eli-abdi <e.abdi02198@gmail.com>
2025-07-23 12:05:44 +0330

    تغییر BrowserRouter به HashRouter برای دیپلوی

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ThemeProvider dir='rtl'>
-   <BrowserRouter>
-   <App />
-   </BrowserRouter>
-  </ThemeProvider>
-);
 
+import { HashRouter } from 'react-router-dom';
+
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
